6ae091ff49dd0db209ae3624952017e79f621061
Jaden505 <jadenxxxl@gmail.com>
2025-07-14 12:34:57 +0200

    add collisions, ground and well exectued jump

--- Game/game.py
+++ Game/game.py
@@ -12,16 +12,24 @@ class Game:
         
         self.screen = pygame.display.set_mode((1200, 800))
         self.clock = pygame.time.Clock()
-        self.player = Player((1100, 700), dt)
+        self.player = Player((1100, 600), dt)
+        self.ground = pygame.Rect(0, 700, 1200, 100)
+        
+        self.sprite_width, self.sprite_height = self.player.sprite.get_width(), self.player.sprite.get_height()+1
         
     def run(self):
         while True:
             self.screen.fill((240,240,240)) # Beige background
             
-            self.player.move_controls()
-            self.player.update_sprite()
+            player_r = pygame.Rect(self.player.x, self.player.y, 
+                                   self.sprite_width, self.sprite_height)
+            
             self.screen.blit(self.player.sprite, (self.player.x, self.player.y)) # Draw sprite 
-             
+            pygame.draw.rect(self.screen, (245, 141, 86), self.ground) # Draw ground
+            
+            self.player.move_controls(player_r.colliderect(self.ground))
+            self.player.update_sprite()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
--- Game/player.py
+++ Game/player.py
@@ -21,7 +21,9 @@ class Player:
         
         self.x, self.y = init_pos[0], init_pos[1]
         self.vx, self.vy = 0, 0
-        self.acx, self.acy = 140, 140
+        self.acx, self.acy = 200, 150
+        
+        self.jump_gravity = 8
         
         self.walk_cycle_ind = 0
         self.push_cycle_ind = 0
@@ -33,6 +35,8 @@ class Player:
     def update_sprite(self):
         if not any(self.moving) and not self.jumping:
             self.sprite = self.stand_left if self.facing_left else self.stand_right
+        elif self.jumping:
+            self.sprite = self.jump_left if self.facing_left else self.jump_right
         else:
             self.anim_timer += self.dt
             
@@ -50,6 +54,10 @@ class Player:
             elif event.key == pygame.K_RIGHT:
                 self.moving[1] = True
                 self.facing_left = False
+            elif event.key == pygame.K_SPACE and not self.jumping:
+                self.jumping = True
+                self.vy = -self.acy
+                self.move_controls(False)
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
@@ -58,13 +66,20 @@ class Player:
                 self.moving[1] = False
 
         
-    def move_controls(self):     
+    def move_controls(self, touching_ground):     
         if self.moving[0]:
             self.vx = -self.acx * self.dt
         elif self.moving[1]:
             self.vx = self.acx * self.dt
         else:
             self.vx = 0
+                        
+        if touching_ground:
+            self.vy = 0
+            self.jumping = False
+        else:
+            self.vy += self.jump_gravity
                 
         self.x += self.vx * self.dt
+        self.y += self.vy * self.dt
             
